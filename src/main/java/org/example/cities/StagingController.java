25b92a36f41fe347f6880316d7f3825c594811a6
cmanikandan <mani.chandra@gmail.com>
2015-04-13 16:01:07 +0530

    change timestamp function

--- src/main/java/org/example/cities/StagingController.java
+++ src/main/java/org/example/cities/StagingController.java
@@ -3,12 +3,16 @@ package org.example.cities;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
 public class StagingController {
 
     @RequestMapping("/")
     public String index() {
-        String timestamp = System.getProperty("staging.timestamp");
+        //String timestamp = System.getProperty("staging.timestamp");
+	String timestamp = 
+	    new java.text.SimpleDateFormat("MM/dd/yyyy h:mm:ss a").format(new Date());
         return "Application was staged at: " + timestamp;
     }
 }
